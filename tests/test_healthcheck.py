fb34dc18ede963a63feb8dd0911c47d81a096fa7
Bernardo Heynemann <heynemann@gmail.com>
2013-06-20 10:26:51 -0300

    tested configuration in server

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -5,6 +5,7 @@ from tornado.testing import AsyncHTTPTestCase
 from preggy import expect
 
 from tests.sandbox.server import SandboxServer
+from tests.sandbox.config import Config
 
 
 class TestHealthCheck(AsyncHTTPTestCase):
@@ -15,3 +16,14 @@ class TestHealthCheck(AsyncHTTPTestCase):
         response = self.fetch('/healthcheck')
         expect(response.code).to_equal(200)
         expect(response.body).to_be_like('WORKING')
+
+
+class TestHealthCheckWithCustomString(AsyncHTTPTestCase):
+    def get_app(self):
+        cfg = Config(HEALTHCHECK_TEXT='SOMETHING ELSE')
+        return SandboxServer(config=cfg).application
+
+    def test_healthcheck(self):
+        response = self.fetch('/healthcheck')
+        expect(response.code).to_equal(200)
+        expect(response.body).to_be_like('SOMETHING ELSE')
