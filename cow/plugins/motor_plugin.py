7f79a53dc921a8ce29a83a3269adc638bd6340e4
Bernardo Heynemann <heynemann@gmail.com>
2013-07-01 22:18:11 -0300

    relase 0.1.6

--- cow/plugins/motor_plugin.py
+++ cow/plugins/motor_plugin.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+
 import motor
 
 from cow.plugins import BasePlugin
@@ -11,9 +13,11 @@ class MotorPlugin(BasePlugin):
     def after_start(cls, server):
         host = server.application.config.get('MONGOHOST')
         port = server.application.config.get('MONGOPORT')
+        logging.info("Connecting to mongodb at %s:%d" % (host, port))
         server.mongo = motor.MotorClient(host, port).open_sync()
 
     @classmethod
     def before_end(cls, server):
         if hasattr(server, 'mongo'):
+            logging.info("Disconnecting from mongodb...")
             server.mongo.disconnect()
